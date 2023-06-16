e96a3d2b0e1d4e8c8a1a3f3e53e4d87d23479ab0
300406uchman <uchee0070@gmail.com>
2023-06-16 11:40:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,16 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
-int main(void);
+/*main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
 {
-    const char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-    int fd = open("/dev/stderr", O_WRONLY);
-    write(fd, message, sizeof(message) - 1);
-    close(fd);
-    return 1;
+const char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+int fd = open("/dev/stderr", O_WRONLY);
+write(fd, message, sizeof(message) - 1);
+close(fd);
+return (0);
 }
 
