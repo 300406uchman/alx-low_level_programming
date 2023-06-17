5f4ef36a0ad10ff6827d8128da0806de723fecb5
300406uchman <uchee0070@gmail.com>
2023-06-17 08:43:25 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,13 @@
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-/*main - Entry point
+
+/**
+ * main - Entry point
  *
  * Return: Always 0(Success)
  */
 int main(void)
 {
-const char message[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-int fd = open("/dev/stderr", O_WRONLY);
-write(fd, message, sizeof(message) - 1);
-close(fd);
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59),
+return (1);
 }
 
