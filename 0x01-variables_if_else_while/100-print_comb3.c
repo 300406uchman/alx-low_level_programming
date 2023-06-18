f5fa75bbb68b973fc2c45d513b8b59cb27db41cf
300406uchman <uchee0070@gmail.com>
2023-06-18 09:07:29 +0100

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i, e;
+for (i = 0; i < 9; i++)
+{
+for (e = i + 1; e < 9; e++)
+{
+putchar(i + '0');
+putchar(e + '0');
+if (i != 8 | e != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar ('\n');
+return (0);
+}
