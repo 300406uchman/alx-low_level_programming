3a49024f1d03fdb9fb2b605c300e14a52eeb5819
300406uchman <uchee0070@gmail.com>
2023-06-20 15:04:00 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: 0 Success
+ *
+ */
+
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+}
+
+int main()
+{
+print_alphabet();
+return (0);
+}
+
