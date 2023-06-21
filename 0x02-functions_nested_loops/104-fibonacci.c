b7326ccd0e2e4ea509b46d0a8418e336c2f306c8
300406uchman <uchee0070@gmail.com>
2023-06-21 09:29:26 +0100

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,35 +11,26 @@
 int main(void)
 {
 unsigned long int i;
-unsigned long int bef = 1;
-unsigned long int aft = 2;
-unsigned long int bef1;
-unsigned long int bef2;
-unsigned long int aft1;
-unsigned long int aft2;
-unsigned long int carry = 0;
+unsigned long int bef[2] = {1, 2};
+unsigned long int aft[2];
 unsigned long int l = 1000000000;
-printf("%lu", bef);
+printf("%lu", bef[0]);
 for (i = 1; i < 91; i++)
 {
-printf(", %lu", aft);
-aft += bef;
-bef = aft - bef;
+printf(", %lu", bef[1]);
+aft[0] = bef[1];
+aft[1] = bef[0] + bef[1];
+bef[0] = aft[0];
+bef[1] = aft[1];
 }
-bef1 = bef / l;
-bef2 = bef % l;
-aft1 = aft / l;
-aft2 = aft % l;
 for (i = 92; i < 99; i++)
 {
-carry = (bef2 + aft2) / l;
-printf(", %lu", (bef1 + aft1 + carry));
-printf("%lu", (bef2 + aft2) % l);
-aft1 = bef1; bef1 = aft1 - bef1 - carry;
-aft2 = bef2 + aft2;
-bef2 = aft2 - bef2;
+printf(", %lu%09lu", bef[1] / l, bef[1] % l);
+aft[0] = bef[1] / l + bef[0] / l;
+aft[1] = (bef[1] % l) + (bef[0] % l);
+bef[0] = aft[0];
+bef[1] = aft[1];
 }
 printf("\n");
 return (0);
 }
-
