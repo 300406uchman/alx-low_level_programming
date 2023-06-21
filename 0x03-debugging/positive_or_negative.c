49b3ec915ff4a541f6b309c330606396935c3bbc
300406uchman <uchee0070@gmail.com>
2023-06-21 18:38:57 +0100

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include "main.h"
 
-void positive_or_negative(int i) {
-	    if (i > 0) {
-		            printf("%d is positive\n", i);
-			        } else if (i < 0) {
-					        printf("%d is negative\n", i);
-						    } else {
-							            printf("The number is zero\n");
-								        }
+/**
+ ** positive_or_negative(int i)- to determine the value of a number
+ ** @i: the number to evakuate
+ **/
+
+void positive_or_negative(int i)
+{
+if (i > 97)
+{
+printf("%d is positive\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+else
+{
+printf("The number is zero\n");
+}
 }
 
