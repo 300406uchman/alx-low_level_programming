b1f5f4b69bc43011503cce18e7cbaedc3dce406e
300406uchman <uchee0070@gmail.com>
2023-09-01 19:40:38 +0100

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ ** my_function - Brief description of what the function does.
+ ** @var1: Description of parameter 1.
+ ** @var2: Description of parameter 2.
+ **
+ ** Return: Description of the return value.
+ **/
+void my_function(int var1, int var2)
+{
+int my_variable = var1 + var2;
+if (my_variable > 0)
+{
+printf("Value is positive.\n");
+}
+else
+{
+printf("Value is non-positive.\n");
+}
+}
